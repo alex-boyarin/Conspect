59f10cff067a1fad2017616bf736e1b1aa7eec09
Aleksandr Boyarin <a.Boyarin86@gmail.com>
2022-03-29 20:14:10 +0300

    update 14

--- src/test/java/api/http/HttpGetExample.java
+++ src/test/java/api/http/HttpGetExample.java
@@ -1,4 +1,4 @@
-package api;
+package api.http;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
@@ -9,7 +9,7 @@ import org.apache.http.util.EntityUtils;
 import java.io.IOException;
 
 //Эти классы писать в MAIN src
-public class ApiExample {
+public class HttpGetExample {
     public static void main(String[] args) throws IOException {
         CloseableHttpClient client = HttpClients.createDefault();// CloseableHttpClient-базавая реализ. интерф. HttpClients
         //   создается обьект http клиента
@@ -17,11 +17,11 @@ public class ApiExample {
         HttpResponse response = client.execute(httpGet);//(execute-выполнять) получаем ответ
         if (response.getStatusLine().getStatusCode() != 200) {//при помощи этих методов получ. статус код запроса
             throw new RuntimeException("massage" +          //выбрасываем новое искл. если статус не равен который мы хотим
-                                       response.getStatusLine().getStatusCode()
-                                       + " " +
+                                       response.getStatusLine().getStatusCode() + " " +
                                        response.getStatusLine().getReasonPhrase());
         }
-        String responseText = EntityUtils.toString(response.getEntity());
+        String responseText = EntityUtils.toString(response.getEntity());//если в запросе было тело, то его можно взять
+        //с помощью response.getEntity() дальше можно привести к стринге и посмотреть на текст ответа.
         System.out.println(responseText);
         client.close();
     }
---
+++ src/test/java/api/taskExample/TokenResponse.java
@@ -0,0 +1,122 @@
+package api.taskExample;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class TokenResponse {
+
+    @JsonProperty("access_token")
+    private String accessToken;
+
+    @JsonProperty("token_type")
+    private String tokenType;
+
+    @JsonProperty("refresh_token")
+    private String refreshToken;
+
+    @JsonProperty("expires_in")
+    private String expiresIn;
+
+    @JsonProperty("scope")
+    private String scope;
+
+    @JsonProperty("user_name")
+    private String userName;
+
+    @JsonProperty("user_id")
+    private String userId;
+
+    @JsonProperty("active")
+    private boolean active;
+
+    @JsonProperty("jti")
+    private String jti;
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public String getTokenType() {
+        return tokenType;
+    }
+
+    public void setTokenType(String tokenType) {
+        this.tokenType = tokenType;
+    }
+
+    public String getRefreshToken() {
+        return refreshToken;
+    }
+
+    public void setRefreshToken(String refreshToken) {
+        this.refreshToken = refreshToken;
+    }
+
+    public String getExpiresIn() {
+        return expiresIn;
+    }
+
+    public void setExpiresIn(String expiresIn) {
+        this.expiresIn = expiresIn;
+    }
+
+    public String getScope() {
+        return scope;
+    }
+
+    public void setScope(String scope) {
+        this.scope = scope;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public String getJti() {
+        return jti;
+    }
+
+    public void setJti(String jti) {
+        this.jti = jti;
+    }
+
+    @Override
+    public String toString() {
+        return "TokenResponse{" +
+               " accessToken='" + accessToken + '\n' +
+               ", tokenType='" + tokenType + '\n' +
+               ", refreshToken='" + refreshToken + '\n' +
+               ", expiresIn='" + expiresIn + '\n' +
+               ", scope='" + scope + '\n' +
+               ", userName='" + userName + '\n' +
+               ", userId='" + userId + '\n' +
+               ", active=" + active +'\n'+
+               ", jti='" + jti +
+               '}';
+    }
+}
